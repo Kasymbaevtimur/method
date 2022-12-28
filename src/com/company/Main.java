bcd57477d1e157bf235eb5d20df285c0033cf980
Kasymbaevtimur <105876764+Kasymbaevtimur@users.noreply.github.com>
2022-12-28 21:12:56 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        String word = "Hello";
+        String word2 = "Hello";
+
+        System.out.println(word.indexOf("o"));
+
+
+
+
+    }
+
+}
+
+
+
